3e6161af626cb76542b372fd1b0f6e45f05d583c
Honey Patel <honey.p@simformsolutions.com>
2025-10-07 11:27:45 +0530

    Add GraphQL index file to export fragment masking and gql utilities

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,24 +1,14 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "http://localhost:4000/graphql/",
-  documents: ["src/**/*.{ts,tsx}"],
+  schema: "https://graphql.org/graphql/",
+  documents: ["src/**/*.tsx"],
   ignoreNoDocuments: true,
   generates: {
-    "./src/__generated__/graphql.tsx": {
-      plugins: [
-        "typescript",
-        "typescript-operations",
-        // "typescript-react-apollo",
-      ],
+    "./src/graphql/": {
+      preset: "client",
       config: {
-        withHooks: false, // Disable hook generation
-        withHOC: false, // Disable HOC generation
-        withComponent: false, // Disable component generation
-        skipTypename: false,
-        withResultType: true,
-        documentVariablePrefix: "",
-        documentVariableSuffix: "Document",
+        documentMode: "string",
       },
     },
   },
